b740d261eef84340f16808301382e687a84d1180
PaulLinca <paultudorlinca@gmail.com>
2020-04-28 14:53:50 +0300

    Added initial application

---
+++ Application/src/main/java/ro/utcn/HomePage.java
@@ -0,0 +1,20 @@
+package ro.utcn;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomePage extends Application
+{
+    public void start(Stage stage) throws Exception
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/home_page.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("20 Questions");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
